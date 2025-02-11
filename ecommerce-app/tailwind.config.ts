17bcb6f0304ae7255e004806a5dd5b90978f1d3a
Stefan Stevic <stefan@steviss.dev>
2025-02-11 11:17:07 +0100

    chore: add husky, nextjs config, some prettier lints

--- ecommerce-app/tailwind.config.ts
+++ ecommerce-app/tailwind.config.ts
@@ -1,19 +1,15 @@
-import type { Config } from "tailwindcss";
-import daisyUI from "daisyui";
+import daisyUI from 'daisyui'
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [daisyUI],
-} satisfies Config;
+} satisfies Config
