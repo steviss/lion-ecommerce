4e663b7a88fbb16b8e26cf83bd5e11074c98391b
Stefan Stevic <stefan@steviss.dev>
2025-02-13 14:29:14 +0100

    chore: chagne flag to false, we always want to affect latest data without delay

--- ecommerce-app/src/lib/sanity.ts
+++ ecommerce-app/src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
-  useCdn: true, // set to `false` to bypass the edge cache
+  useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: '2025-02-06',
   token: process.env.SANITY_API_TOKEN,
 })
